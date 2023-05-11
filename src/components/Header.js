230c8a97a049281a0dfcd79a30726da49175c793
zdenekdev <kucera.contact@gmail.com>
2023-05-11 17:28:33 +0200

    mobile menu a úprava fontů

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+
+import React, { useState } from "react";
 
 function Header() {
+  const [menuActive, setMenuActive] = useState("header__mobile-nav");
+
+  const menuOpen = () => {
+    menuActive === "header__mobile-nav"
+      ? setMenuActive("header__mobile-nav header__mobile-nav--active")
+      : setMenuActive("header__mobile-nav");
+  };
+
   return (
     <div className="header">
-      <h3 className="header__title">Zdeněk Kučera</h3>
+      <h2 className="header__title">Zdeněk Kučera</h2>
       <ul className="header__nav">
         <li>
           <a className="header__nav-item" href="#home">
@@ -28,6 +38,63 @@ function Header() {
           </a>
         </li>
       </ul>
+      <div className="header__mobile-menu">
+        <i className="header__hamburger" onClick={menuOpen}>
+          {/* <Bars3Icon className="header__hamburger-icon" /> */}
+          <FontAwesomeIcon className="header__hamburger-icon" icon={faBars} />
+        </i>
+        <div className={menuActive}>
+          <FontAwesomeIcon
+            onClick={menuOpen}
+            className="header__mobile-nav-exit"
+            icon={faXmark}
+          />
+          <div className="header__mobile-nav-container">
+            <ul className="header__mobile-nav-list">
+              <li className="header__mobile-nav-item">
+                <a
+                  onClick={menuOpen}
+                  className="header__mobile-nav-link"
+                  href="#home"
+                  rel="noreferrer"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="header__mobile-nav-item">
+                <a
+                  onClick={menuOpen}
+                  className="header__mobile-nav-link"
+                  href="#about"
+                  rel="noreferrer"
+                >
+                  About
+                </a>
+              </li>
+              <li className="header__mobile-nav-item">
+                <a
+                  onClick={menuOpen}
+                  className="header__mobile-nav-link"
+                  href="#projects"
+                  rel="noreferrer"
+                >
+                  Projects
+                </a>
+              </li>
+              <li className="header__mobile-nav-item">
+                <a
+                  onClick={menuOpen}
+                  className="header__mobile-nav-link"
+                  href="#contacts"
+                  rel="noreferrer"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
