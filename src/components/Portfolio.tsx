988001857c04a081b338dbb67e7354e4662dc186
zdenekdev <kucera.contact@gmail.com>
2024-05-27 19:55:29 +0200

    zdraveinfo project

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import zdraveInfo from "../images/projects/zdraveinfo.webp";
 import standUp from "../images/projects/stand-up.webp";
 import itunesPlaylist from "../images/projects/itunes-playlist.webp";
 import amazonProject from "../images/projects/amazon-project.webp";
@@ -13,6 +14,9 @@ import nextAuthLogo from "../images/next-auth.svg";
 import reduxLogo from "../images/redux.svg";
 import reactLogo from "../images/react.svg";
 import sassLogo from "../images/sass.svg";
+import wordpressLogo from "../images/wordpress.svg";
+import phpLogo from "../images/php.svg";
+import bootstrapLogo from "../images/bootstrap.svg";
 import { Tooltip } from "react-tooltip";
 import Reveal from "./utils/Reveal";
 
@@ -32,10 +36,90 @@ function Portfolio() {
               <div className="portfolio__project">
                 <div className="portfolio__img-container">
                   <a
+                    href="https://zdraveinfo.cz/"
+                    target="_blank"
+                    rel="noreferrer">
+                    <img
+                      className="portfolio__img"
+                      src={zdraveInfo}
+                      alt="project website"
+                    />
+                    <div className="portfolio__in-progress">
+                      Project in progress...
+                    </div>
+                  </a>
+                </div>
+
+                <div className="portfolio__text">
+                  <h3 className="portfolio__project-title">Zdraveinfo.cz</h3>
+
+                  <p className="portfolio__description">
+                    Encyclopedia of herbal medicine. Search for herbs by name or
+                    health benefits and share your experiences with the
+                    community. The application allows you to create a user
+                    account to save your favorite herbs and make personal notes.
+                    I used WordPress CMS, leveraged WP REST API and created a
+                    custom theme.
+                  </p>
+
+                  <ul className="portfolio__stack">
+                    <li className="portfolio__tech">
+                      <img
+                        className="portfolio__tech-img"
+                        src={wordpressLogo}
+                        alt="wordpress_logo"
+                        data-tooltip-id="wordpress"
+                        data-tooltip-content="WordPress"
+                      />
+                      <Tooltip id="wordpress" />
+                    </li>
+                    <li className="portfolio__tech">
+                      <img
+                        className="portfolio__tech-img portfolio__tech-php"
+                        src={phpLogo}
+                        alt="php_logo"
+                        data-tooltip-id="php"
+                        data-tooltip-content="PHP"
+                      />
+                      <Tooltip id="php" />
+                    </li>
+                    <li className="portfolio__tech">
+                      <img
+                        className="portfolio__tech-img portfolio__tech-bootstrap"
+                        src={bootstrapLogo}
+                        alt="bootstrap_logo"
+                        data-tooltip-id="bootstrap"
+                        data-tooltip-content="Bootstrap"
+                      />
+                      <Tooltip id="bootstrap" />
+                    </li>
+                  </ul>
+
+                  <div className="portfolio__links">
+                    <a
+                      className="portfolio__demo-link"
+                      href="https://zdraveinfo.cz/"
+                      target="_blank"
+                      rel="noreferrer">
+                      Live page
+                      <i>
+                        <FontAwesomeIcon
+                          className="portfolio__arrow-icon"
+                          icon={faArrowUpRightFromSquare}
+                        />
+                      </i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </Reveal>
+            <Reveal>
+              <div className="portfolio__project">
+                <div className="portfolio__img-container">
+                  <a
                     href="https://stand-up-events.vercel.app/"
                     target="_blank"
-                    rel="noreferrer"
-                  >
+                    rel="noreferrer">
                     <img
                       className="portfolio__img"
                       src={standUp}
@@ -104,8 +188,7 @@ function Portfolio() {
                       className="portfolio__github-link"
                       href="https://github.com/zdenekdev/stand-up"
                       target="_blank"
-                      rel="noreferrer"
-                    >
+                      rel="noreferrer">
                       Code
                       <i>
                         <FontAwesomeIcon
@@ -118,8 +201,7 @@ function Portfolio() {
                       className="portfolio__demo-link"
                       href="https://stand-up-events.vercel.app/"
                       target="_blank"
-                      rel="noreferrer"
-                    >
+                      rel="noreferrer">
                       Live demo
                       <i>
                         <FontAwesomeIcon
@@ -138,8 +220,7 @@ function Portfolio() {
                   <a
                     href="https://itunes-playlist.vercel.app/"
                     target="_blank"
-                    rel="noreferrer"
-                  >
+                    rel="noreferrer">
                     <img
                       className="portfolio__img"
                       src={itunesPlaylist}
@@ -199,8 +280,7 @@ function Portfolio() {
                       className="portfolio__github-link"
                       href="https://github.com/zdenekdev/itunes-playlist"
                       target="_blank"
-                      rel="noreferrer"
-                    >
+                      rel="noreferrer">
                       Code
                       <i>
                         <FontAwesomeIcon
@@ -213,8 +293,7 @@ function Portfolio() {
                       className="portfolio__demo-link"
                       href="https://itunes-playlist.vercel.app/"
                       target="_blank"
-                      rel="noreferrer"
-                    >
+                      rel="noreferrer">
                       Live demo
                       <i>
                         <FontAwesomeIcon
@@ -233,8 +312,7 @@ function Portfolio() {
                   <a
                     href="https://amzn-project.vercel.app/"
                     target="_blank"
-                    rel="noreferrer"
-                  >
+                    rel="noreferrer">
                     <img
                       className="portfolio__img"
                       src={amazonProject}
@@ -320,8 +398,7 @@ function Portfolio() {
                       className="portfolio__github-link"
                       href="https://github.com/zdenekdev/amazon-project"
                       target="_blank"
-                      rel="noreferrer"
-                    >
+                      rel="noreferrer">
                       Code
                       <i>
                         <FontAwesomeIcon
@@ -334,8 +411,7 @@ function Portfolio() {
                       className="portfolio__demo-link"
                       href="https://amzn-project.vercel.app/"
                       target="_blank"
-                      rel="noreferrer"
-                    >
+                      rel="noreferrer">
                       Live demo
                       <i>
                         <FontAwesomeIcon
